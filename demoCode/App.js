71d2082907f0d174ee55a59a41c4190c00068c33
linhua hu <linhuahu@linhuadeMacBook-Pro.local>
2019-04-24 17:59:38 +0800

    creater prj

---
+++ demoCode/App.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import TimerButton from "rn-time-button";
+
+const instructions = Platform.select({
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TimerButton
+          enable={true}
+          style={{
+            width: 150,
+            height: 45,
+            paddingRight: 10,
+            borderWidth: 1,
+            borderColor: "red"
+          }}
+          textStyle={{ color: "red" }}
+          timerCount={60}
+          onClick={shouldStartCountting => {
+            shouldStartCountting(true);
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
